3c2978a83cac9635b6ec9677debca13cd1967145
Christopher Mühl <christopher@padarom.xyz>
2020-07-10 15:06:49 +0200

    Use correct types for matrix apps

--- src/awtrix/matrix.ts
+++ src/awtrix/matrix.ts
@@ -1,22 +1,41 @@
 import Channel from './communication/channels/Channel'
-// @ts-ignore
 import App, { Command } from '@awtrix/app'
-import logger from '../utils/logger'
+
+// We need to create a couple of helper types so that we get the type for
+// the non-abstract version of our abstract app. Otherwise we won't be able
+// to construct it from the user's config, as abstract classes can't be
+// constructed.
+type AbstractConstructorHelper<T> = (new (...args: any) => { [x: string]: any; }) & T;
+type AbstractContructorParameters<T> = ConstructorParameters<AbstractConstructorHelper<T>>;
+type AppParameters = AbstractContructorParameters<typeof App>
+type ImplementedApp = { new(...a: AppParameters): App }
+
+// The type of the module returned by `app/fixtures/index.js`.
+type UserConfig = {
+  config: any,
+  apps: {
+    [k: string]: ImplementedApp | { default: ImplementedApp }
+  }
+}
 
 export default class Matrix {
+  width = 32
+  height = 8
+
   apps: App[] = []
   activeApp = 0
+
   frameInterval?: NodeJS.Timeout
 
   constructor (protected channel: Channel, protected homeDir: string) { }
 
   async loadApps () {
-    let apps = require(this.homeDir)
+    let config = require(this.homeDir) as UserConfig
 
-    apps.forEach((app: any) => {
-      if (app.default) app = app.default
+    Object.values(config.apps).forEach((ctor) => {
+      if (typeof ctor === 'object') ctor = ctor.default
 
-      const initializedApp = new app(this, { 'showSeconds': true })
+      const initializedApp = new ctor(this, { 'showSeconds': true })
       initializedApp.setup()
       this.apps.push(initializedApp)
     })
