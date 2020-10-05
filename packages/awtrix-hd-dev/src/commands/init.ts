c18bc7fafe6a37462a666a7fa63570dd7daccef8
Christopher Mühl <christopher@padarom.xyz>
2020-10-06 00:01:10 +0200

    Configure start script

--- packages/awtrix-hd-dev/src/commands/init.ts
+++ packages/awtrix-hd-dev/src/commands/init.ts
@@ -1,4 +1,5 @@
 import { Command, flags } from '@oclif/command'
+// @ts-ignore
 import Yeoman from 'yeoman-environment'
 import Generator from '../generator'
 
--- packages/awtrix-hd-dev/src/commands/start.ts
+++ packages/awtrix-hd-dev/src/commands/start.ts
@@ -1,22 +1,35 @@
 import { Command, flags } from '@oclif/command'
 import { Command as AwtrixStartCommand } from 'awtrix-hd'
 import fs from 'fs'
+import path from 'path'
+// @ts-ignore
+import copyTemplateDir from 'copy-template-dir'
+import buildBackend from '../packer/buildBackend'
+import buildFrontend from '../packer/buildFrontend'
+import createPackage from '../packer/createPackage'
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log(promise)
+})
 
 export default class Start extends Command {
   static description = 'starts an Awtrix HD server with only the app in the current folder configured'
 
-  static flags = AwtrixStartCommand.flags
+  static flags = {}
 
   async run() {
     try {
       const packageJson = fs.readFileSync('package.json', 'utf-8')
-      const config = JSON.parse(packageJson)
+      let config = JSON.parse(packageJson)
+
+      await this.createAwtrixHome(config)
+
+      AwtrixStartCommand.run(['--home', './.awtrix', '--production'])
     } catch (error) {
       this.error('Could not find a "package.json" in your current directory.')
     }
 
     // 1. Start watcher that compiles sources (extract from pack.ts)
-    // 2. Configure home directory to be in working directory
     // 3. Symlink watcher output (dist/) to the awtrix app directory
     // 4. Create awtrix configuration that includes the current app
     // 5. Start awtrix
@@ -25,8 +38,17 @@ export default class Start extends Command {
     // TODO: Find out how to handle automatic reloading for assets
     // TODO: Find out how to use hot reloading for compiled frontend component
 
-    // TODO: Find out how to properly pass through args to delegate this command
-    // call directly to Awtrix HD.
-    AwtrixStartCommand.run(process.argv.slice(3))
+  }
+
+  async createAwtrixHome (config: any): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const source = path.join(__dirname, '../../templates', 'development')
+      const variables = { name: config.name, version: config.version }
+
+      copyTemplateDir(source, '.awtrix', variables, (error: Error | null) => {
+        if (error) return reject(error)
+        resolve()
+      })
+    })
   }
 }
