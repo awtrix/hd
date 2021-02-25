c336914f711f30171bc1a34c4cee139f4d68b253
Christopher Mühl <christopher@padarom.xyz>
2021-02-25 23:27:04 +0100

    Add first app settings

---
+++ packages/awtrix/web/components/Settings/Inputs/Dropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ name }}:
+    <select>
+      <option v-for="option in options.options" :key="option">{{ option }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
