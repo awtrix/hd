36b70b40b6baa2fca2f362c659753ccdcfec6991
Christopher Mühl <christopher@padarom.xyz>
2021-04-08 23:20:20 +0200

    Vue 3 Typescript frontend

--- packages/common/src/createFrontend.ts
+++ packages/common/src/createFrontend.ts
@@ -1,6 +1,28 @@
+import {
+  defineComponent,
+  ComponentOptionsWithoutProps,
+  ComponentOptionsWithObjectProps,
+  ComponentOptionsWithArrayProps,
+  ComponentOptionsMixin,
+  EmitsOptions
+} from 'vue'
+import FrontendApp from './FrontendApp'
+
+function foobar<A, B, C, D, E, F, G, H, K, J>
+  (options?: ComponentOptionsWithoutProps<{}, {}, {}, {}, {}, ComponentOptionsMixin, typeof FrontendApp, EmitsOptions, string>): GeneratorType
+
+// TODO: Determine whether we want to remove the extends field from the options' ThisType.
+//       Otherwise a user might accidentally use `extends: FooBar` and no longer have proper types.
+function foobar(options?: any): GeneratorType {
+  const generatorFunction = (base: typeof FrontendApp) => defineComponent({ ...options, extends: base })
+
+  return generatorFunction as GeneratorType
+}
+
+export { foobar }
+
 import { ThisTypedComponentOptionsWithRecordProps, ThisTypedComponentOptionsWithArrayProps } from 'vue/types/options'
 import { VueConstructor } from 'vue/types/vue'
-import FrontendApp from './FrontendApp'
 
 // `Vue.extend` returns a `VueConstructor<T>`. We want to know what T stands for in this case,
 // since this matches the instance type (i.e. `this` when used inside the component).
@@ -19,10 +41,10 @@ function extend<Data, Methods, Computed, Props>
   (options?: ThisTypedComponentOptionsWithRecordProps<InstanceType, Data, Methods, Computed, Props>): GeneratorType
 
 function extend(options?: any): GeneratorType {
-  const generatorFunction = (base: typeof FrontendApp) => FrontendApp.extend(options)
+  const generatorFunction = (base: typeof FrontendApp) => base.extend(options)
 
   // `vue-loader` creates a `render` function and attaches it to the exported function's
-  // `options` property. If the exported function is a Vue constructor that is fine, we
+  // `options` property. If the exported function is a Vue constructor that is fine. We
   // are dealing with our custom generator function however, so we need to add the
   // `options` property as well. We will then override the actual render function after
   // loading the component in our frontend.
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,18 @@
 export { GeneratorType, default as createFrontend } from './createFrontend'
 export { default as BackendApp } from './BackendApp'
 export { default as FrontendApp } from './FrontendApp'
+import { foobar } from './createFrontend'
 export * as Types from './types'
+
+foobar({
+  data () {
+    return {
+      nuddel: 'foa'
+    }
+  },
+
+  created () {
+    this.nuddel
+    this.lock()
+  }
+})
